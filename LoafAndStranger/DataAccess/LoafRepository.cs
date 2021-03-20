4393c45b91e25259b00d4df2c78764ddb41879bf
Dana Pham <danapham12@gmail.com>
2021-03-20 11:42:08 -0500

    updated with Dapper stuff from class

--- LoafAndStranger/DataAccess/LoafRepository.cs
+++ LoafAndStranger/DataAccess/LoafRepository.cs
@@ -1,116 +1,65 @@
-﻿using LoafAndStranger.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LoafAndStranger.Models;
 using Microsoft.Data.SqlClient;
+using Dapper;
 
 namespace LoafAndStranger.DataAccess
 {
     public class LoafRepository
     {
         const string ConnectionString = "Server=localhost;Database=LoafAndStranger;Trusted_Connection=True;";
-        static List<Loaf> _loaves = new List<Loaf>
-        {
-            new Loaf { Id = 1, Price = 5.50m, Size = LoafSize.Medium, Sliced = true, Type = "Rye" },
-            new Loaf { Id = 3, Price = 2.50m, Size = LoafSize.Small, Sliced = false, Type = "French" }
-        };
 
         public List<Loaf> GetAll()
         {
-            var loaves = new List<Loaf>();
-
             //create a connection
-            using var connection = new SqlConnection(ConnectionString);
-
-            //open the connection
-            connection.Open();
-
-            //create a command
-            var command = connection.CreateCommand();
+            using var db = new SqlConnection(ConnectionString);
 
             //telling the command what you want to do
-            var sql = @"Select *
+            var sql = @"Select * 
                         From Loaves";
-            command.CommandText = sql;
 
-            //send the command to sql
-            //execute the command
-            var reader = command.ExecuteReader();
-
-            //loop over our results
-            while (reader.Read()) //reader.Read pulls one row at a time from the db
-            {
-                var loaf = MapLoaf(reader);
-                //add it to the list
-                loaves.Add(loaf);
-            }
-
-            return loaves;
+            return db.Query<Loaf>(sql).ToList();
         }
 
         public void Add(Loaf loaf)
         {
-            var biggestExistingId = _loaves.Max(l => l.Id);
-            loaf.Id = biggestExistingId + 1;
-            _loaves.Add(loaf);
+            var sql = @"INSERT INTO [Loaves] ([Size],[Type],[WeightInOunces],[Price],[Sliced])
+                        OUTPUT inserted.Id
+                        VALUES(@Size, @type, @weightInOunces, @Price, @Sliced)";
+
+            using var db = new SqlConnection(ConnectionString);
+
+            var id = db.ExecuteScalar<int>(sql, loaf);
+
+            loaf.Id = id;
         }
 
         public Loaf Get(int id)
         {
-            var sql = $@"SELECT *
-                        FROM Loaves
+            var sql = @"Select *
+                        From Loaves
                         where Id = @id";
 
             //create a connection
-            using var connection = new SqlConnection(ConnectionString);
-            connection.Open();
+            using var db = new SqlConnection(ConnectionString);
 
-            //create a command
-            var command = connection.CreateCommand();
-            command.CommandText = sql;
-            command.Parameters.AddWithValue("id", id);
+            var loaf = db.QueryFirstOrDefault<Loaf>(sql, new { id = id });
 
-            //execute the command
-            var reader = command.ExecuteReader();
-
-            if (reader.Read())
-            {
-                var loaf = MapLoaf(reader);
-                return loaf;
-            }
-
-            return null;
+            return loaf;
         }
 
         public void Remove(int id)
         {
-            var loafToRemove = Get(id);
-            _loaves.Remove(loafToRemove);
-        }
+            var sql = @"Delete 
+                        from Loaves 
+                        where Id = @id";
 
-        private Loaf MapLoaf(SqlDataReader reader)
-        {
-            var id = (int)reader["Id"]; //explicit cast (throws exceptions)
-            var size = (LoafSize)reader["Size"];
-            var type = reader["Type"] as string; //implicit cast (returns null, thus can't be used on value types)
-            var weightInOunces = (int)reader["WeightInOunces"];
-            var price = (decimal)reader["price"];
-            var sliced = (bool)reader["Sliced"];
-            var createdDate = (DateTime)reader["CreatedDate"];
+            using var db = new SqlConnection(ConnectionString);
 
-            //make a loaf
-            var loaf = new Loaf
-            {
-                Id = id,
-                Price = price,
-                Size = size,
-                Type = type,
-                Sliced = sliced,
-                WeightInOunces = weightInOunces,
-            };
-
-            return loaf;
+            db.Execute(sql, new { id });
         }
     }
 }
--- LoafAndStranger/Models/Loaf.cs
+++ LoafAndStranger/Models/Loaf.cs
@@ -1,14 +1,16 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace LoafAndStranger.Models
 {
+    //Models are for storing pieces of information, not for having behavior
     public class Loaf
     {
-        //Models are for storing pieces of information, not for having behavior
         public int Id { get; set; }
+
         public LoafSize Size { get; set; }
         public string Type { get; set; }
         public decimal Price { get; set; }
