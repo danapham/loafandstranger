78e9dee041abe33d87ab514e6e292e4c9bed720c
Dana Pham <danapham12@gmail.com>
2021-03-23 18:55:28 -0500

    entity framework for Tops data

---
+++ LoafAndStranger/DataAccess/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using LoafAndStranger.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoafAndStranger.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Loaf> Loaves { get; set; }
+        public DbSet<Top> Tops { get; set; }
+        public DbSet<Stranger> Strangers { get; set; }
+    }
+}
--- LoafAndStranger/Startup.cs
+++ LoafAndStranger/Startup.cs
@@ -33,6 +33,8 @@ namespace LoafAndStranger
 
             services.AddSingleton(Configuration);
             services.AddTransient<StrangersRepository>();
+
+            services.AddDbContext<AppDbContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
